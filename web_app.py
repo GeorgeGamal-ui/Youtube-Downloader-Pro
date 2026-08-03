80fbdd5dcd8a20dee29d927b76e9bb61b10d55f7
GeorgeGamal-ui <georgegamalsamer@gmail.com>
2026-08-04 02:04:42 +0300

    web_app.py

--- web_app.py
+++ web_app.py
@@ -208,7 +208,7 @@ if st.button(text[lang]["btn_download"], use_container_width=True):
                 'logger': StreamlitLogger(),
                 'progress_hooks': [my_hook],
                 'extractor_args': {'youtube': ['client=ios,android,tv']},
-                'js_runtimes': {'nodejs': {}}
+                'js_runtimes': {'node': {}}
             }
 
             # قراءة الكوكيز من إعدادات Streamlit بأمان لتخطي حظر يوتيوب
