eb2a390c5d0a3a6426d9b2698e1e0bbd1567b7e5
GeorgeGamal-ui <georgegamalsamer@gmail.com>
2026-08-04 02:29:47 +0300

    web_app.py

--- web_app.py
+++ web_app.py
@@ -214,7 +214,7 @@ if st.button(text[lang]["btn_download"], use_container_width=True):
                 'no_warnings': True,
                 'logger': StreamlitLogger(),
                 'progress_hooks': [my_hook],
-                'js_runtimes': {'deno': DENO_PATH}
+                'js_runtimes': {'deno': {}}
             }
 
             if "youtube_cookies" in st.secrets:
