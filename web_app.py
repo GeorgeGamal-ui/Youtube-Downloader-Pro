74baa2c66f2f6ef8b35d62c9e9f9ddc1012ee93b
GeorgeGamal-ui <georgegamalsamer@gmail.com>
2026-08-04 01:47:24 +0300

    web_app.py

--- web_app.py
+++ web_app.py
@@ -1,9 +1,9 @@
+import tempfile
+import os
 import streamlit as st
 import yt_dlp
-import os
-import tempfile
 import shutil
-import re 
+import re
 
 # إعدادات صفحة الموقع
 st.set_page_config(page_title="YouTube Downloader PRO", page_icon="⬇️", layout="centered")
@@ -197,13 +197,15 @@ if st.button(text[lang]["btn_download"], use_container_width=True):
                 'quiet': True,
                 'no_warnings': True,
                 'logger': StreamlitLogger(),
-                'progress_hooks': [my_hook],
-                # استخدام عميل التلفزيون والأيفون لتخطي حظر السيرفرات
-                'extractor_args': {'youtube': ['client=tv,ios']},
-                # إرسال بيانات وهمية كأننا شاشة سمارت
-                'http_headers': {
-                    'User-Agent': 'Mozilla/5.0 (SMART-TV; Linux; Tizen 5.0) AppleWebKit/538.1 (KHTML, like Gecko) Version/5.0 NativeTV/2.0 SmartTV Safari/538.1'
-                }
+                'progress_hooks': [my_hook]
+            }
+
+            # الكود الجديد لقرائة الكوكيز بأمان من السيرفر
+            if "youtube_cookies" in st.secrets:
+                cookie_fd, cookie_path = tempfile.mkstemp(suffix=".txt")
+                with os.fdopen(cookie_fd, 'w') as f:
+                    f.write(st.secrets["youtube_cookies"])
+                opts['cookiefile'] = cookie_path
             }
             
             if format_choice == text[lang]["vid_format"]:
